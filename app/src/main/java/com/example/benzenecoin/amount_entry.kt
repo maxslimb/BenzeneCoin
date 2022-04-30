a688530895ca1876f922c66a89bfc0133f500781
Prathmesh Pandey <prathmeshpandey9@gmail.com>
2022-04-30 15:27:41 +0530

    transaction_activity_2 and amount_entry_activity

---
+++ app/src/main/java/com/example/benzenecoin/amount_entry.kt
@@ -0,0 +1,11 @@
+package com.example.benzenecoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class amount_entry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_amount_entry)
+    }
+}
---
+++ app/src/main/java/com/example/benzenecoin/transaction_activity_2.kt
@@ -0,0 +1,11 @@
+package com.example.benzenecoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class transaction_activity_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction2)
+    }
+}
