254fcd26f5172cb601c7498ee245799901de5c91
maxslimb <kpatel3032@gmail.com>
2022-05-01 01:26:38 +0530

    implemented permissions

--- app/src/main/java/com/example/benzenecoin/RecieveActivity.kt
+++ app/src/main/java/com/example/benzenecoin/RecieveActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
-class receiving : AppCompatActivity() {
+class RecieveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiving)
