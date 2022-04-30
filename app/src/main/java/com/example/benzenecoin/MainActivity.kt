a21cab51633365cef15b15ac9853597aa065413b
Prathmesh Pandey <prathmeshpandey9@gmail.com>
2022-05-01 00:49:06 +0530

    nearby_api

--- app/src/main/java/com/example/benzenecoin/MainActivity.kt
+++ app/src/main/java/com/example/benzenecoin/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.benzenecoin
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.web3j.protocol.Web3j
@@ -25,6 +28,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, receiving::class.java)
             startActivity(intent)
         }
+
+        val wifi: WifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifi.isWifiEnabled = true
+        wifi.isWifiEnabled = false
+
+        val bt_send = findViewById<Button>(R.id.send)
+        val bt_receive = findViewById<Button>(R.id.receive)
+        val balance = findViewById<TextView>(R.id.Balance)
+        val sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor= sharedPreferences.edit()
+        if(!sharedPreferences.getBoolean("updateb_once",false)) {
+            editor.putBoolean("updateb_once", true)
+            editor.putString("balance", "2000")
+            editor.commit()
+        }
+        balance.text = sharedPreferences.getString("balance","0")
+        CheckPermission()
+
 
 
         val web3 = Web3j.build(HttpService("https://rinkeby.infura.io/v3/03dfd526311a4ce3933474b193a25238"));
