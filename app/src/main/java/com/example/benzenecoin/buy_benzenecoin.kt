8f90f31143957c44ab6ad4cafbe5807244ba329b
maxslimb <kpatel3032@gmail.com>
2022-05-01 04:17:32 +0530

    rest api's implemented

--- app/src/main/java/com/example/benzenecoin/buy_benzenecoin.kt
+++ app/src/main/java/com/example/benzenecoin/buy_benzenecoin.kt
@@ -1,10 +1,18 @@
 package com.example.benzenecoin
 
 import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputEditText
 
 class buy_benzenecoin : AppCompatActivity() {
@@ -17,8 +25,34 @@ class buy_benzenecoin : AppCompatActivity() {
         val b = sharedPreferences.getString("balance","")
         val Balance_rec = findViewById<TextView>(R.id.Balance_bb)
         Balance_rec.text=b
+
+
+// Add the request to the RequestQueue.
+
         buttonClick.setOnClickListener {
            val amount = amount_bb.text.toString();
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://13.234.204.127:5000/transfer"
+            val stringRequest = StringRequest(
+                Request.Method.POST, url,
+                { response ->
+                    // Display the first 500 characters of the response string.
+                    Toast.makeText(applicationContext,"$response",Toast.LENGTH_SHORT).show()
+                    val sharedPreferences = getSharedPreferences("user_data", Context.MODE_PRIVATE)
+                    var b = sharedPreferences.getString("balance","")
+                    if (b != null) {
+                        b = (b.toInt() + amount.toInt()).toString()
+                    }
+                    val editor: SharedPreferences.Editor= sharedPreferences.edit()
+                    editor.putString("balance", b)
+                    editor.commit()
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                },
+                { error -> Toast.makeText(applicationContext,"$error",Toast.LENGTH_SHORT).show()
+                Log.d("kk","$error")})
+            queue.add(stringRequest)
         }
     }
 }
