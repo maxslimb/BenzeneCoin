a33d925bfa09db6d94bfffcdcb50e41340a8550b
Prathmesh Pandey <prathmeshpandey9@gmail.com>
2022-04-30 21:28:37 +0530

    receiving_activity

---
+++ app/src/main/java/com/example/benzenecoin/payment_successful.kt
@@ -0,0 +1,11 @@
+package com.example.benzenecoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class payment_successful : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_successful)
+    }
+}
---
+++ app/src/main/java/com/example/benzenecoin/receiving.kt
@@ -0,0 +1,11 @@
+package com.example.benzenecoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class receiving : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiving)
+    }
+}
