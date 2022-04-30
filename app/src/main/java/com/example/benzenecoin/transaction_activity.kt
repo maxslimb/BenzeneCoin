60a7333140075908f8b905db001eddcf153c2f52
Prathmesh Pandey <prathmeshpandey9@gmail.com>
2022-04-30 14:40:04 +0530

    transaction_activity

---
+++ app/src/main/java/com/example/benzenecoin/transaction_activity.kt
@@ -0,0 +1,11 @@
+package com.example.benzenecoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class transaction_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction)
+    }
+}
